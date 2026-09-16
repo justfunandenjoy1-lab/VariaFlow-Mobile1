ceac4079f4fe3bc100df0d695fa5b2b05ecc2d3c
justfunandenjoy1-lab <justfunandenjoy1@gmail.com>
2026-09-16 16:56:55 +0600

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // FFmpegKit maintained fork repository
+        maven { url = uri("https://repo1.maven.org/maven2") }
+    }
+}
+
+rootProject.name = "VariaFlow Mobile"
+include(":app")
